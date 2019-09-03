4c502bc3eadd7b6b22393487bafc3c48dadc370e
Ghale Madayag <ghale.madayag@gmail.com>
2019-09-03 18:17:28 +0800

    update query id

--- server/profile-handler.php
+++ server/profile-handler.php
@@ -2,7 +2,14 @@
     require_once('handler.php');
 
     if(isset($_POST['get_id'])){
+        $id = $_POST['get_id'];
 
+        $sql = $handler->query("SELET * FROM member WHERE mem_id = '$id'");
+        while ($row = $sql->fetch(PDO::FETCH_OBJ)) {
+            $result[] = array(
+                'id' => $row->mem_id 
+            );
+        }
     }
 
 ?>
