ce33bc8eb0056ca37fab18e8081d7bea31b8c300
Ghale Madayag <ghale.madayag@gmail.com>
2019-09-05 19:03:02 +0800

    dashboard 11

--- server/dashboard-handler.php
+++ server/dashboard-handler.php
@@ -8,16 +8,16 @@
         while ($row = $sql->fetch(PDO::FETCH_OBJ)) {
             $attend = 0;
             $parSql = $handler->prepare("SELECT * FROM participants WHERE mem_id=?");
-            $parSql->execute(array($userId, $row->eve_id));
+            $parSql->execute(array($userId));
 
-            // while($rowPar = $parSql->fetch(PDO::FETCH_OBJ)) {
-            //     //$parEve = $rowPar->eve_id;
-            //     //$eve = $row->eve_id;
+            while($rowPar = $parSql->fetch(PDO::FETCH_OBJ)) {
+                $parEve = $rowPar->eve_id;
+                $eve = $row->eve_id;
 
-            //     // if($parEve==$eve){
-            //     //     $attend = 1;  
-            //     // }     
-            // }
+                if($parEve==$eve){
+                    $attend = 1;  
+                }     
+            }
 
             $result[] = array(
                 'id' => $row->eve_id,
