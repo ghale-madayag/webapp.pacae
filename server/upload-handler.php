fdc11b50f41e39e887ca538f879a20381358fcba
Ghale Madayag <ghale.madayag@gmail.com>
2019-09-17 03:43:53 +0800

    Img V2

--- server/upload-handler.php
+++ server/upload-handler.php
@@ -1,5 +1,7 @@
 <?php
     header('Access-Control-Allow-Origin: *');
-    $new_image_name = urldecode($_FILES["file"]["name"]).".jpg";
+    $new_image_name = urldecode($_FILES["file"]["name"]);
     move_uploaded_file($_FILES["file"]["tmp_name"], "../img/".$new_image_name);
+
+    echo $_POST['value1'];
 ?>
