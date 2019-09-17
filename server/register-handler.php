7ccab7a23c94ee2df5987756b6d697039e0cb763
Ghale Madayag <ghale.madayag@gmail.com>
2019-09-17 19:31:54 +0800

    email verification

--- server/register-handler.php
+++ server/register-handler.php
@@ -2,8 +2,7 @@
     require_once('handler.php');
 
     if(isset($_POST['fname'])){
-        $email = $_POST['email'];
-        $contact = $_POST['mobile'];
+
         
         $sqlCheck = $handler->prepare("SELECT mem_email, mem_contact FROM member WHERE mem_email= ? OR mem_contact= ?");
         $sqlCheck->execute(array($email,$contact));
@@ -11,6 +10,10 @@
         if ($sqlCheck->rowCount()) {
 			echo 0;
 		}else{
+            $email = $_POST['email'];
+            $to = $email;
+            $subject = "PACAE VERIFICATION";
+            $fullname = $_POST['fname'];
             $sql = $handler->prepare('INSERT INTO member(
                 `mem_fname`, 
                 `mem_lname`,
@@ -53,7 +56,25 @@
                 'schooladd' => isset($_POST['schooladd']) ? $_POST['schooladd'] : null
             ));
     
-            echo 1;
+            $headers  = 'MIME-Version: 1.0' . "\r\n";
+            $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+    
+            $message = '<html><body>';
+            $message .= '<h1>ACCOUNT VERIFICATION REMINDER</h1>';
+            $message .= '<p>Dear '.$fullname.'</p>';
+            $message .= '<p>Thank You!<br/>Your registration to PACAE App has been approved. You are now advised to Log-in using your encoded username and password.
+            </p> ';
+            $message .= '<p>Email: '.$email.'</p>';
+            $message .= '<p>Password: <strong>'.$pword.'</strong></p>';
+            $message .= '<br/>';
+            $message .= '<br/>';
+            $message .= '</body></html>';
+    
+            if(mail($to, $subject, $message, $headers)){
+                echo 1;
+            } else{
+               echo 0;
+            }
         }
     }
 ?>
