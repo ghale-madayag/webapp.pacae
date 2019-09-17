43d6225d2ec240855b62f6a561ad985aaffd9778
Ghale Madayag <ghale.madayag@gmail.com>
2019-09-17 08:58:15 +0800

    img v4

--- server/upload-handler.php
+++ server/upload-handler.php
@@ -1,7 +1,11 @@
 <?php
+    require_once('handler.php');
     header('Access-Control-Allow-Origin: *');
     $new_image_name = urldecode($_FILES["file"]["name"]);
     move_uploaded_file($_FILES["file"]["tmp_name"], "../img/".$new_image_name);
 
-    echo $_POST['value1'].' '.$_POST['value2'];
+    $sql = $handler->prepare("UPDATE participants SET par_status=1 WHERE eve_id=? AND mem_id=?");
+    $sql->execute(array($_POST['value1'], $_POST['value2']));
+
+    echo 1;
 ?>
