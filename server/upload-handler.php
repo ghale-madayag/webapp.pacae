641e68915db936967e4686dd239ef16363e4ad2a
Ghale Madayag <ghale.madayag@gmail.com>
2019-09-17 09:16:39 +0800

    img v6

--- server/upload-handler.php
+++ server/upload-handler.php
@@ -4,8 +4,8 @@
     $new_image_name = urldecode($_FILES["file"]["name"]);
     move_uploaded_file($_FILES["file"]["tmp_name"], "../img/".$new_image_name);
 
-    $sql = $handler->prepare("UPDATE participants SET par_status=1 WHERE eve_id=? AND mem_id=?");
-    $sql->execute(array($_POST['value1'], $_POST['value2']));
+    $sql = $handler->prepare("UPDATE participants SET par_status=1,par_img=? WHERE eve_id=? AND mem_id=?");
+    $sql->execute(array($new_image_name,$_POST['value1'], $_POST['value2']));
 
-    echo $_FILES["file"]["tmp_name"];
+    echo 1;
 ?>
